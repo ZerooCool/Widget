6022c5a64daed7e849edc25c0b70da8913ff5cf9
Bernard <g2pc@visionduweb.com>
2014-12-20 01:38:50 +0100

    Mise à niveau du fichier de parsing PHP SimpleXML. Ajout de la possibilité Data Islands a tester. Ajout du fichier decouverte-structure.php ( pour la structure XML ) Ajout du XML utilisé actuellement en production. Ajout d'un fichier NotesTMP pense bête facultatif.

--- Widget 00 01/Affichage RSS SimpleXML.php
+++ Widget 00 01/Affichage RSS SimpleXML.php
@@ -17,8 +17,42 @@ try{
 	if(!@$fluxrss=simplexml_load_file('../xml/rss_test_48.xml')){
 		throw new Exception('Flux introuvable');}
 		
-		echo'Code à venir.';
+		/* Récupérer une donnée dans le fichier XML devient facile sous forme de tableau. */
+		/* Exemple pour $fluxrss->channel->title qui va renvoyer : */
+		/* Le contenu de la balise title, avec pour parent « channel »  lui même ayant pour parent $fluxrss qui est l’élement racine rss. */
+		/* Pour connaitre le contenu de l’objet $fluxrss : echo ('var_dump($fluxrss)'); */
+
+
+		/* Si le fichier n’a pas été trouvé, un ‘!’ lance une Exception qui arrête le script et renvoie au Bloc catch. */
+		/* Utilisation de la fonction empty, si les données sont vides une erreur est renvoyée.  */
+		/* La fonction empty peut être remplacée par la fonction is_string qui spécifie à PHP le type de contenu "chaîne de caractères". */
+		if(!is_string($fluxrss->channel->title) || empty($fluxrss->channel->description) || empty($fluxrss->channel->item->title)) throw new Exception('Flux invalide');
+		echo '<h3>'.$fluxrss->channel->title.'</h3><p>'.$fluxrss->channel->description.'</p>';
 		
+		
+		
+		/*  A FAIRE, Le controle sur les string à la place des empty.
+		La question du string !
+		// if(isset($fluxrss->channel->title) && is_string($fluxrss->channel->title) && str_lenght($fluxrss->channel->title)>=4)
+		
+		
+		$ctrl1 = (isset($fluxrss->channel->title) && is_string($fluxrss->channel->title) && str_lenght($fluxrss->channel->title)>=4)?"oui":"non";
+		echo  $ctrl1;
+		$ctrl2 = (isset($fluxrss->channel->title) && is_string($fluxrss->channel->title) && str_lenght($fluxrss->channel->title)>=4) ;
+		echo  $ctrl2;
+
+		----------------------->
+		
+		$ctrl1 = (isset($fluxrss->channel->title) && is_string($fluxrss->channel->title) && str_length($fluxrss->channel->title)>=4)?$fluxrss->channel->title:null;
+		if(null!==$ctrl1){
+			//work
+		}
+		
+		 */
+		
+		
+		
+		echo'Fin.<br/><br/>Source : http://www.snoupix.com/lire-un-flux-rss-avec-simplexml_tutorial_17.html';
 }
 
 
@@ -26,3 +60,8 @@ try{
 /* Bloc catch qui va afficher le message d'erreur. */
 catch(Exception $e){echo $e->getMessage();}
 ?>
+
+
+
+
+
---
+++ Widget 00 01/decouverte-structure.php
@@ -0,0 +1,44 @@
+<!--
+Simple script pour découvrir la structure du fichier XML
+
+Solution automatique qui déroule un fichier .xml pour identifier les éléments de la structure : tables, champs et données.
+Le résultat du parser construit le tableau associatif qui permet de manipuler les ‘tables’, ‘éléments’ et ‘données’.
+-->
+
+<?php
+//Script xml_parser.php?file='fichier_test.xml'
+
+$file = ( !empty($_GET['file']) ? $_GET['file'] : '../xml/rss_test_48.xml');
+$tb = array();
+
+function pere_ou_fils( $_elt ){
+	global $tb;
+	$tb['TABLES'][] = strtoupper($_elt->getName());
+	foreach( $_elt as $_pere => $_fils ){
+		
+		if ( count( $_fils->children() ) > 0 ){
+			pere_ou_fils( $_fils );}
+			
+			else {
+				$tb['CHAMPS'][strtoupper($_elt->getName())][]=strtoupper($_pere);
+				$_donnee=$_elt->$_pere;
+				$tb['DONNEES'][strtoupper($_elt->getName())][]=$_donnee;
+			}
+	}
+}
+
+if (file_exists( $file ) ){
+	$xml = simplexml_load_file( $file );
+	pere_ou_fils($xml);
+	$xml='';
+	echo '<pre>';
+	print_r($tb);
+	echo '</pre>';}else{
+		exit( '<pre>Echec lors de l\'ouverture du fichier '.$file.'</pre>' );}
+?>
+
+<!--
+Le lien suivant complète le code ci-dessus.
+Il permet d'exploiter le contenu d'un fichier XML, toujours avec SimpleXML.
+http://www.aurelienpiat.com/2011/03/25/traiter-simplement-un-fichier-xml-avec-simplexml-en-php/
+-->
