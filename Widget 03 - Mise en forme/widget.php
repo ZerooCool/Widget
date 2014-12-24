5ae49c6312fe0fe87f7f9e446a64cfd1a8d083f8
Bernard <g2pc@visionduweb.com>
2014-12-24 03:03:29 +0100

    Reporting NotesTMP.txt
    Avancement CSS de base.
    
    Affichage d'un 3ème Widget dans widget.php ( + widget.css )
    
    Objectif : Paramétrer les tailles sur des critères standards.

--- Widget 03 - Mise en forme/widget.php
+++ Widget 03 - Mise en forme/widget.php
@@ -1,45 +1,27 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr" dir="ltr">
 <head>
-    <title>Widget RSS</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Widget RSS</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
 <!-- Avec la bonne DTD les bords sont bien arrondis. -->
-	
+
 <!--  Intégrer Widget.css -->
-	<link rel="stylesheet" href="widget.css" />
+<link rel="stylesheet" href="widget.css" />
 
 <!--  Intégrer Widget.js -->
-	<script type="text/javascript" src="widget.js"></script>
+<script type="text/javascript" src="widget.js"></script>
 
 </head>
 <body>
 
-<H3><?php 
-echo("Hello Widget !");
-?></H3>
 
-
-<!--  Sources du Widget 1 en exemple. -->
-<script type="text/javascript">
-var pausecontent=new Array()
-pausecontent[0]='<a href="http://www.oxado.com/?origin=pub15190" target="_blank">Oxado</a><br />Webmasters, gagnez de l\'argent en affichant des bannières contextuelles Oxado'
-pausecontent[1]='<a href="http://www.atleticwong.be" target="_blank">AtleticWong.Be</a><br />Une équipe de football...'
-pausecontent[2]='<a href="http://www.espacejavascript.com/DESIGN/chartegraphique.php">Charte Graphique</a><br />Besoin d\'une charte graphique ?.'
-pausecontent[3]='<a href="http://www.espacejavascript.com/DESIGN/chartegraphique.php">Graphique</a><br />Besoin d\'une charte graphique ?.'
-pausecontent[4]='<a href="http://www.espacejavascript.com/DESIGN/chartegraphique.php">Charte</a><br />Besoin d\'une charte graphique ?.'
-pausecontent[5]='<a href="http://www.espacejavascript.com/DESIGN/chartegraphique.php">Chaque</a><br />Besoin d\'une charte graphique ?.'
-</script>
-
-
-<!-- >>> Ci-dessous il faut récupérer les informations depuis le fichier XML. <<< -->
-<!--  Sources du Widget 2 a adapter. -->
+<!-- Sources des 3 Widgets. -->
 <?php 
 
 /* ############################## */
 /* # Encapsulation dans le try. # */
 /* ################################################################################################################################## */
-/* Début */
 
 try{
 	/* La fonction simplexml_load_file permet de charger un fichier XML. */
@@ -50,57 +32,65 @@ try{
 		throw new Exception('Flux introuvable');}
 		
 		/* Utilisation de la fonction empty, si les données sont vides une erreur est renvoyée.  */
-		/* Un contrôle sur is_string peut être envisagé plutôt que d'utiliser empty. */
 		if(empty($fluxrss->channel->title) || empty($fluxrss->channel->description) || empty($fluxrss->channel->item->title)) throw new Exception('Flux invalide');
 		
-			/* Affichage test du titre principale du Flux RSS */
+			/* Affichage test du titre principale et de la description du Flux RSS */
 			/* echo '<h3>'.$fluxrss->channel->title.'</h3>'; */
-		
-			/* Affichage test de la description principale du Flux RSS */
 			/* echo ('<p>'.$fluxrss->channel->description.'</p>'); */
 
-			/* Début du Javascript qui permet l'affichage du parser dans sa box. */
-			echo '<script type="text/javascript">';
-			echo 'var pausecontentNew=new Array();';
+				/* Début du Javascript qui permet l'affichage du contenu du parser dans sa box via JavaScript. */
+				echo '<script type="text/javascript">';
+				
+					/* Afficher plusieurs Widgets */
+					echo 'var widget01=new Array();';
+					echo 'var widget02=new Array();';
+					echo 'var widget03=new Array();';
+
 /* ######################################################### */					
 /* # Boucle pour afficher le contenu dans le Widget xHTML. # */
 /* ################################################################################################################################## */
-/* Début */
 
-$i = 0; /* Déclaration de la variable $i permettant de boucler et affectation de la valeur 0 */
+				$i = 0; /* Déclaration de la variable $i permettant de boucler et affectation de la valeur 0 */
 				$nb_affichage = 100; /* Déclaration de la variable $nb_affichage et affectation de la valeur 05*/
 
 					/* Boucle pour afficher l'ensemble des informations du fichier .xml*/
 					foreach($fluxrss->channel->item as $item){
-						
+
 						/* IMPORTANT */
 						/* Permet d'ajouter la date de création de l'annonce, mais, certaines dates sont de 1970 sur le fichier actuel rss_48.xml */
 						/* La fonction date prend en paramètre un format et un timestamp. La fonction strtotime convertit la date pudDate en timestamp. */
 						/* echo('<i>publié le'.(string)date('d/m/Y à G\hi',strtotime($item->pubDate)).'</i>'); echo('<br/>'); */
-						
+
 						/* Le parsing et son rendu doit donner le modèle ci-dessous */
-						/* pausecontentNew[0]='<a href="LIEN" target="_offres-emploi">TITRE DU LIEN</a><br />DESCRIPTION' */						
+						/* Variable[0]='<a href="LIEN" target="_offres-emploi">TITRE DU LIEN</a><br />DESCRIPTION' */						
 
-						/* Création du lien et du titre du lien. Ajout du title et de la target. Ajout de la description */
+						/* Création du lien avec title et target. Titre de l'offre. Description */
 						/* addslashes rajoute l'échappement des caractères attendu par le script JavaScript. */
-						echo 'pausecontentNew['.$i.']=\'<a href="'.(string)$item->link.'" target="_offres-emploi" title="'.addslashes((string)$item->title).'">'.addslashes((string)$item->title).'</a><br />'.addslashes((string)$item->description).'\'';
 						
-						/* Ajoute un retour chariot, pour un affichage propre du rendu du code source. */
-						echo "\r";
+						/* Widget01 : Parser et création des variables pour le script JavaScript du Widget01. */
+						echo 'widget01['.$i.']=\'<a href="'.(string)$item->link.'" target="_offres-emploi" title="'.addslashes((string)$item->title).'">'.addslashes((string)$item->title).'</a><br />'.addslashes((string)$item->description).'\'';
+						echo "\r"; /* Ajoute un retour chariot, pour un affichage propre du rendu du code source. */
 
+						/* Widget02 : Parser et création des variables pour le script JavaScript du Widget02. */
+						echo 'widget02['.$i.']=\'<a href="'.(string)$item->link.'" target="_offres-emploi" title="'.addslashes((string)$item->title).'">'.addslashes((string)$item->title).'</a><br />'.addslashes((string)$item->description).'\'';
+						echo "\r"; /* Ajoute un retour chariot, pour un affichage propre du rendu du code source. */
+
+						/* Widget03 : Parser et création des variables pour le script JavaScript du Widget03. */
+						echo 'widget03['.$i.']=\'<a href="'.(string)$item->link.'" target="_offres-emploi" title="'.addslashes((string)$item->title).'">'.addslashes((string)$item->title).'</a><br />'.addslashes((string)$item->description).'\'';
+						echo "\r"; /* Ajoute un retour chariot, pour un affichage propre du rendu du code source. */
+						
 					if(++$i>=$nb_affichage)
 					break;
 			 		}
-	 		
+
 /* Fin de la boucle pour afficher le contenu dans le Widget xHTML. */
 /* ################################################################################################################################## */
 
-			echo '</script>';
-
-			
+				echo '</script>';
+			 		
 /* Fin d'encapsulation dans le try. */
 /* ################################################################################################################################## */
-		 		
+
 }
 
 
@@ -110,43 +100,48 @@ catch(Exception $e){echo $e->getMessage();}
 ?>
 
 
-
-<!-- Titre fixe du Widget 1 -->
+<!-- Titre statique du Widget01 -->
 <h3 style="width:200px; color:#ffffff; background-color:#990000; margin:0; text-align:center; padding:5px; border-radius: 10px 10px 0 0; border: 1px solid #ccc;">Offres en cours</h3>
 
-<!-- Affichage du Widget 1 -->
+<!-- Affichage du Widget01 -->
 <div>
 <script type="text/javascript">
-new pausescroller(pausecontent, "pscroller1", "someclass", 2000)
+new pausescroller(widget01, "pscroller01", "someclass", 2000)
 </script>
 </div>
 
 
 
-<br/><br/>
+<br/>###########SEPARATION###########<br/><br/>
 
 
 
-<!-- Titre fixe du Widget 2 -->
+<!-- Titre statique du Widget02 -->
 <h3 style="width:200px; color:#ffffff; background-color:#990000; margin:0; text-align:center; padding:5px; border-radius: 10px 10px 0 0; border: 1px solid #ccc;">Offres en cours</h3>
 
-<!-- Affichage du Widget 2 -->
+<!-- Affichage du Widget02 -->
 <div>
 <script type="text/javascript">
-new pausescroller(pausecontentNew, "pscroller2", "someclass", 2500)
+new pausescroller(widget02, "pscroller02", "someclass", 2500)
 </script>
 </div>
 
 
 
-<!-- Code exemple pour afficher les 2 Widget -->
-<!-- new pausescroller(name_of_message_array, CSS_ID, CSS_classname, pause_in_miliseconds) -->
+<br/>###########SEPARATION###########<br/><br/>
 
-<!-- <script type="text/javascript"> -->
-<!-- new pausescroller(pausecontent, "pscroller1", "someclass", 2000) -->
-<!-- new pausescroller(pausecontentNew, "pscroller2", "someclass", 3000) -->
-<!-- ... -->
-<!-- </script> -->
+
+
+<!-- Titre statique du Widget03 -->
+<h3 style="width:200px; color:#ffffff; background-color:#990000; margin:0; text-align:center; padding:5px; border-radius: 10px 10px 0 0; border: 1px solid #ccc;">Offres en cours</h3>
+
+<!-- Affichage du Widget03 -->
+<div>
+<script type="text/javascript">
+new pausescroller(widget03, "pscroller03", "someclass", 2500)
+</script>
+</div>
+
 
 </body>
 </html>
